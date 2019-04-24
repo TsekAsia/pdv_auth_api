54bf6711641e0ecfc96cf1dd366b5e57158a9164
Marc Dagatan <marc.dagatan@gmail.com>
2019-04-24 15:59:23 +0800

    authenticate request before action

--- app/controllers/pdv_auth_api/api/auth_controller.rb
+++ app/controllers/pdv_auth_api/api/auth_controller.rb
@@ -1,6 +1,16 @@
 module PdvAuthApi
   module Api
     class AuthController < ApiController
+      before_action :authenticate_request
+
+    private
+
+    def authenticate_request
+      auth_request = AuthorizeApiRequest.new(headers: request.headers)
+
+      return if auth_request.valid?
+
+      render json: { errors: auth_request.errors }, status: :unauthorized
     end
   end
 end
