631fb727e78ca3dcc437753620109629bef7277f
Marc Dagatan <marc.dagatan@gmail.com>
2019-04-25 12:55:47 +0800

    update and save actions for accounts

--- lib/pdv_auth_api/v1/account.rb
+++ lib/pdv_auth_api/v1/account.rb
@@ -5,6 +5,10 @@ module PdvAuthApi
                     :id, :first_name, :middle_name, :last_name, :email,
                     :created_at, :updated_at, :username
 
+      EDITABLE_ATTRIBUTES = %i[
+        first_name middle_name last_name email username
+      ].freeze
+
       def initialize(**params)
         assign_attributes(params)
       end
@@ -24,6 +28,34 @@ module PdvAuthApi
           @errors = body[:errors]
           false
         end
+      end
+
+      def update(**params)
+        sanitized_params = {
+          user: params.select { |key, _val| EDITABLE_ATTRIBUTES.include?(key) }
+        }.to_json
+
+        @response = authenticated_api.patch 'account', sanitized_params
+        body = JSON.parse(@response.body, symbolize_names: true)
+
+        if @response.status == 200
+          assign_attributes(body)
+          @user = body
+          self
+        else
+          @errors = body.errors
+          false
+        end
+      end
+
+      def save
+        new_attrs = {}
+
+        EDITABLE_ATTRIBUTES.each do |key|
+          send(key) != user[:"#{key}"] ? new_attrs[:"#{key}"] = send(key) : nil
+        end
+
+        update(new_attrs)
       end
 
       def authenticated_api
