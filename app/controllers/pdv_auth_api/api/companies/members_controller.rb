2ba0eca26014c396b7699944084c73eefc3994d6
Marc Dagatan <marc.dagatan@gmail.com>
2019-04-27 17:08:49 +0800

    set_company before any action

--- app/controllers/pdv_auth_api/api/companies/members_controller.rb
+++ app/controllers/pdv_auth_api/api/companies/members_controller.rb
@@ -2,9 +2,17 @@ module PdvAuthApi
   module Api
     module Companies
       class MembersController < ApiController
+        before_action :set_company
+
         def index; end
 
         def batch; end
+
+        private
+
+        def set_company
+          @company.find(params[:company_id])
+        end
       end
     end
   end
