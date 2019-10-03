b89850134a76ccd9d8cafecac09b2c3ed201dd3c
Marc Dagatan <marc.dagatan@gmail.com>
2019-10-03 15:49:21 +0800

    Add membership attribute to account attr_accessor

--- lib/pdv_auth_api/v1/account.rb
+++ lib/pdv_auth_api/v1/account.rb
@@ -3,7 +3,7 @@ module PdvAuthApi
     class Account
       attr_accessor :token, :user, :response, :errors,
                     :id, :first_name, :middle_name, :last_name, :email,
-                    :created_at, :updated_at, :username
+                    :created_at, :updated_at, :username, :membership
 
       EDITABLE_ATTRIBUTES = %i[
         first_name middle_name last_name email username
