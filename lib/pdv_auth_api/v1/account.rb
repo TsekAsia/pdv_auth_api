2627ff9c7f8cea8d21b74d5c8d19110f7b6a2303
Richard Angelo Praxedes <richard@fullsuite.ph>
2019-10-07 12:35:40 +0800

    Add disabled_at field to account

--- lib/pdv_auth_api/v1/account.rb
+++ lib/pdv_auth_api/v1/account.rb
@@ -3,7 +3,8 @@ module PdvAuthApi
     class Account
       attr_accessor :token, :user, :response, :errors,
                     :id, :first_name, :middle_name, :last_name, :email,
-                    :created_at, :updated_at, :username, :membership
+                    :created_at, :updated_at, :username, :membership,
+                    :disabled_at
 
       EDITABLE_ATTRIBUTES = %i[
         first_name middle_name last_name email username
