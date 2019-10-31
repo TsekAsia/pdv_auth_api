b5290bc37edae86f9a35092ecc94b12edb166711
Richard Angelo Praxedes <richard@fullsuite.ph>
2019-10-31 16:12:12 +0800

    Add app spec

---
+++ spec/lib/app_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe PdvAuthApi::V1::App do
+  let(:email) { 'test@testmail.dev' }
+  let(:password) { 'thisisthepassword' }
+  let(:account) { PdvAuthApi::V1::Account.new }
+  let(:app_id) { 1 }
+
+  describe 'find' do
+    it 'finds the app' do
+      VCR.use_cassette('auth_login_valid_login') do
+        VCR.use_cassette('app_find_success') do
+          app = PdvAuthApi::V1::App.find(id: app_id)
+        end
+      end
+    end
+  end
+end
